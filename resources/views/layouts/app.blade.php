def276a64a04e8d8bcd1edb80c89420a63ae21db
gagahfyd <gagahfajar46@gmail.com>
2026-01-08 03:20:04 +0700

    fix css serving

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/css/app.css">
+    
     <style>
         :root {
             --bg-primary: #F5F5F1;
