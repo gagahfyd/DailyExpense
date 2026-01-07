efe93bf3455f736d3e43cf8a8d6f245806cddbef
gagahfyd <gagahfajar46@gmail.com>
2026-01-08 02:26:45 +0700

    Fix asset loading with Vite and prepare Railway build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         :root {
             --bg-primary: #F5F5F1;
